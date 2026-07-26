5da4c95bbf9787595d24f4aac370b19569adf9bc
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-26 08:41:32 +0700

    refactor(mksh): update error handling for error when changing permission

--- mksh.sh
+++ mksh.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 NAMEFILE=$1
+OPEN_SCRIPT=true
 
 if [[ -z "$NAMEFILE" ]]; then
   echo "Please input file name first"
   exit 1
 else
+  # Check suffix
   if [[ "$NAMEFILE" != *.sh ]]; then
     FINALNAME="$NAMEFILE.sh"
   else
     FINALNAME="$NAMEFILE"
   fi
-  
+
+  # Initialize script
   echo "#!/bin/bash" > "$FINALNAME"
-  chmod +x "$FINALNAME"
-  echo "Creating script completed!"
-  echo "Opening file..."
-  vim "$FINALNAME"
+  if chmod +x "$FINALNAME"; then
+    echo "Creating script completed!"
+  else
+    echo "Creating script success but there's a problem while update script permission"
+  fi
+
+  # Auto open script
+  if [[ $OPEN_SCRIPT ]]; then
+    echo "Opening file..."
+    vim "$FINALNAME"
+  fi
   echo "Saved as $FINALNAME"
 
 fi
